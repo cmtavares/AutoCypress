bd1c8c380d26305ed71fc810ccc2527b284d7bc5
Carlos Tavares <cmmt08@gmail.com>
2024-01-19 12:43:42 -0300

    alteracoes nos testes automatizados

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="Cypress" />
+
+context('Funcionalidade Login', () =>{
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    it('Deve fazer login com sucesso', () =>{
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+    })
+
+    it('Deve exibir uma mensagem de erro', () =>{
+        cy.get('#username').type('xxxx@xxxx.com')
+        cy.get('#password').type('xxxxx123')
+        cy.get('.woocommerce-form > .button').click()
+    })
+})
